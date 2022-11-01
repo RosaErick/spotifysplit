6bacde96ceaf3a6b8e1e1c5d09d89f782d472fe3
Erick Rosa <erickpmotta@gmail.com>
2022-11-01 03:04:55 -0300

    setting up env files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import 'dotenv/config';
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const port = process.env.PORT || 3000;
+
 
 const app = express();
-const port = process.env.PORT || 3000;
+
 
 app.get("/", (req, res) => {
   res.send("health check");
 });
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
