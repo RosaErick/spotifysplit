f080c26d37f689b1112c27d12d9d3c1e24cbb741
Erick Rosa <erickpmotta@gmail.com>
2022-11-05 00:53:09 -0300

    adding axios and new auth routes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,107 @@
 import express from "express";
-import 'dotenv/config';
+import "dotenv/config";
+import { generateRandomString } from "./utils/common";
+import axios from "axios";
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
-const port = process.env.PORT || 3000;
-
-
-const app = express();
-
 
 app.get("/", (req, res) => {
   res.send("health check");
 });
 
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+  const stateKey = "spotify_auth_state";
 
+  res.cookie(stateKey, state);
 
+  const searchparams = new URLSearchParams({
+    client_id: CLIENT_ID,
+    response_type: "code",
+    redirect_uri: REDIRECT_URI,
+    scope: "user-read-private user-read-email",
+    state: state,
+  });
 
+  const queryParams = searchparams.toString();
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.get("/callback", (req, res) => {
+  const code = req.query.code || null;
+
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    params: {
+      code: code,
+      redirect_uri: REDIRECT_URI,
+      grant_type: "authorization_code",
+    },
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        const { access_token, token_type } = response.data;
+
+        axios
+          .get("https://api.spotify.com/v1/me", {
+            headers: {
+              Authorization: `${token_type} ${access_token}`,
+            },
+          })
+          .then((response) => {
+            res.send(`<pre>
+        ${JSON.stringify(response.data, null, 2)}
+      </pre>`);
+          })
+          .catch((error) => {
+            res.send(error);
+          });
+      } else {
+        res.send(response);
+      }
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+app.get("/refresh_token", (req, res) => {
+  const { refresh_token } = req.query;
+
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    params: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    },
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString("base64")}`,
+    },
+  })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
---
+++ src/utils/common.js
@@ -0,0 +1,12 @@
+//generetes random string containg numbers and letters
+
+export const generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+};
