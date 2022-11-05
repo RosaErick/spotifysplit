db32c62b6bc97e6f44fd1377ffdf0db05be61de5
Erick Rosa <erickpmotta@gmail.com>
2022-11-05 01:30:43 -0300

    setting up frontend, installing tailwind and  typescript

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+type Props = {};
+
+export const LoginForm = (props: Props) => {
+  return (
+    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Username
+        </label>
+
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="Username"
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+
+        <input
+          className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          placeholder="******************"
+        />
+      </div>
+
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Sign In
+        </button>
+      </div>
+    </form>
+  );
+};
+
+
