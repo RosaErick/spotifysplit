1eb5028ed9d83c98a80ac81f735d1ead560c2905
Erick Rosa <erickpmotta@gmail.com>
2023-01-23 11:45:14 -0300

    deploy fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom";
-import { AppRoutes } from "./routes/Routes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <AppRoutes />
-      </Router>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </QueryClientProvider>
   );
 }
